32b714aa6cc349d082ef8ecf49f166fec8aa9fc2
Phuc Tran <tranquangphuc@outlook.com>
2024-12-08 10:32:21 +0700

    Chapter 04: Deploying Our Microservices Using Docker

--- test-em-all.bash
+++ test-em-all.bash
@@ -5,18 +5,19 @@
 #   HOST=localhost PORT=7000 ./test-em-all.bash
 #
 : ${HOST=localhost}
-: ${PORT=7000}
+: ${PORT=8080}
 : ${PROD_ID_REVS_RECS=1}
 : ${PROD_ID_NOT_FOUND=13}
 : ${PROD_ID_NO_RECS=113}
 : ${PROD_ID_NO_REVS=213}
 
 function assertCurl() {
+
     local expectedHttpCode=$1
     local curlCmd="$2 -w \"%{http_code}\""
     local result=$(eval $curlCmd)
     local httpCode="${result:(-3)}"
-    RESPONSE='' && (( ${#result} > 3)) && RESPONSE="${result%???}"
+    RESPONSE='' && (( ${#result} > 3 )) && RESPONSE="${result%???}"
 
     if [ "$httpCode" = "$expectedHttpCode" ]
     then
@@ -27,14 +28,15 @@ function assertCurl() {
             echo "Test OK (HTTP Code: $httpCode, $RESPONSE)"
         fi
     else
-        echo "Test FAILED, EXPECTED HTTP Code: $expectedHtpCode, GOT: $httpCode, WILL ABORT!"
-        echo "- Failing command: $curlCmd"
-        echo "- Response Body: $RESPONSE"
+        echo  "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode, WILL ABORT!"
+        echo  "- Failing command: $curlCmd"
+        echo  "- Response Body: $RESPONSE"
         exit 1
     fi
 }
 
 function assertEqual() {
+
     local expected=$1
     local actual=$2
 
@@ -47,19 +49,60 @@ function assertEqual() {
     fi
 }
 
+function testUrl() {
+    url=$@
+    if $url -ks -f -o /dev/null
+    then
+        return 0
+    else
+        return 1
+    fi;
+}
+
+function waitForService() {
+    url=$@
+    echo -n "Wait for: $url... "
+    n=0
+    until testUrl $url
+    do
+        n=$((n + 1))
+        if [[ $n == 100 ]]
+        then
+            echo " Give up"
+            exit 1
+        else
+            sleep 3
+            echo -n ", retry #$n "
+        fi
+    done
+    echo "DONE, continues..."
+}
+
 set -e
+
+echo "Start Tests:" `date`
 
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
+if [[ $@ == *"start"* ]]
+then
+    echo "Restarting the test environment..."
+    echo "$ docker compose down --remove-orphans"
+    docker compose down --remove-orphans
+    echo "$ docker compose up -d"
+    docker compose up -d
+fi
 
-# Verify that a normal request workds, expect three recommendations and three reviews
+waitForService curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS
+
+# Verify that a normal request works, expect three recommendations and three reviews
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS -s"
 assertEqual $PROD_ID_REVS_RECS $(echo $RESPONSE | jq .productId)
 assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
 assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
 
-# Verify that a 404 (Not Found) error is returned for a non-existing productId
+# Verify that a 404 (Not Found) error is returned for a non-existing productId ($PROD_ID_NOT_FOUND)
 assertCurl 404 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NOT_FOUND -s"
 assertEqual "No product found for productId: $PROD_ID_NOT_FOUND" "$(echo $RESPONSE | jq -r .message)"
 
@@ -82,5 +125,12 @@ assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
 # Verify that a 400 (Bad Request) error is returned for a productId that is not a number, i.e. invalid format
 assertCurl 400 "curl http://$HOST:$PORT/product-composite/abc -s"
 assertEqual "\"Method parameter 'productId': Failed to convert value of type 'java.lang.String' to required type 'int'; For input string: \\\"abc\\\"\"" "$(echo $RESPONSE | jq .message)"
+
+if [[ $@ == *"stop"* ]]
+then
+    echo "We are done, stopping the test environment..."
+    echo "$ docker compose down"
+    docker compose down
+fi
 
 echo "End, all tests OK:" `date`
