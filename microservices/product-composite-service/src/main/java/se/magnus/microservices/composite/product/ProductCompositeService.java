32d78af31cdf1c0257952a92e9c8ac1083b1b938
Phuc Quang Tran <me@tranquangphuc.com>
2024-12-23 20:21:30 +0700

    Chapter 05: Adding an API Description Using OpenAPI

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeService.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeService.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import se.magnus.microservices.api.composite.product.ProductAggregate;
 import se.magnus.microservices.api.composite.product.RecommendationSummary;
 import se.magnus.microservices.api.composite.product.ReviewSummary;
@@ -20,6 +24,7 @@ import se.magnus.microservices.composite.product.client.RecommendationService;
 import se.magnus.microservices.composite.product.client.ReviewService;
 import se.magnus.microservices.util.http.ServiceUtil;
 
+@Tag(name = "ProductComposite", description = "REST API for composite product information")
 @RestController
 public class ProductCompositeService {
 
@@ -45,6 +50,16 @@ public class ProductCompositeService {
      * @param productId Id of the product
      * @return the composite product info, if found, else null
      */
+    @Operation(
+        summary = "${api.product-composite.get-composite-product.description}",
+        description = "${api.product-composite.get-composite-product.notes}"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+        @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+        @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+        @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
     @GetMapping(value = "/product-composite/{productId}")
     ProductAggregate getProduct(@PathVariable int productId) {
         Product product = productService.getProduct(productId);
